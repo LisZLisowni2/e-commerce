0eb9945b6a14d3b3027a4f14670063512ba890ee
LisZLisowni2 <rafalkozikowski735@gmail.com>
2026-02-06 10:03:32 +0100

    Main configuration of frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
-
+  import { RouterView, RouterLink } from "vue-router"
 </script>
 
 <template>
-
+  <header class="p-4">
+    <h1 class="text-3xl text-violet-700">E-commerance</h1>
+  </header>
+  <nav class="p-4">
+    <p>Navigation</p>
+  </nav>
+  <main class="bg-zinc-50 min-h-screen">
+    <router-view />
+  </main>
 </template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router/router'
+import { createPinia } from "pinia"
+import { QueryClient, VueQueryPlugin } from "@tanstack/vue-query"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const pinia = createPinia()
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60 * 5,
+            retry: 1
+        }
+    }
+})
+
+app.use(pinia)
+app.use(VueQueryPlugin, { queryClient })
+app.use(router)
+app.mount('#app')
