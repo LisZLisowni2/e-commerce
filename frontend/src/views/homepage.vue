592bcc7790a12c37e5b62fd0459153864e5ab872
LisZLisowni2 <rafalkozikowski735@gmail.com>
2026-03-07 10:29:52 +0100

    Add swiper module

--- frontend/src/views/homepage.vue
+++ frontend/src/views/homepage.vue
@@ -1,7 +1,38 @@
 <script setup lang="ts">
+    import { Swiper, SwiperSlide } from "swiper/vue"
+    import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules"
+    
+    import "swiper/css"
+    import "swiper/css/navigation"
+    import "swiper/css/scrollbar"
+    import "swiper/css/pagination"
 
+    const modules = [Navigation, Pagination, Scrollbar, A11y]
+
+    const onSwiper = (swiper) => {
+        console.log(swiper)
+    }
+
+    const onSlideChange = () => {
+        console.log("Slide changed")
+    }
 </script>
 
 <template>
-    <h1>Home page</h1>
+    <section class="w-full p-10">
+        <swiper
+            :slides-per-view="1"
+            :space-between="50"
+            navigation
+            :modules="modules"
+            :pagination="{ clickable: true }"
+            :scrollbar="{ draggable: true }"
+            @swiper="onSwiper" 
+            @slide-change="onSlideChange"   
+        >
+            <swiper-slide><div class="p-10 flex flex-row justify-center">Slide 1</div></swiper-slide>
+            <swiper-slide>Slide 2</swiper-slide>
+            <swiper-slide>Slide 3</swiper-slide>
+        </swiper>
+    </section>
 </template>
